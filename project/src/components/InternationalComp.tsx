5f5358f9c70540261ba6ddc6fc13e22945c8156f
BommaBalaji <bommabalaji70@gmail.com>
2024-10-21 23:39:23 +0530

    collaboration images added

--- project/src/components/InternationalComp.tsx
+++ project/src/components/InternationalComp.tsx
@@ -7,6 +7,8 @@ import Img5 from '../Assets/Images/img5.png';
 import Img6 from '../Assets/Images/img6.png';
 import Img7 from '../Assets/Images/img7.png';
 import Img8 from '../Assets/Images/img8.png';
+import meai from '../Assets/Images/meai.png';
+import imec from '../Assets/Images/imec.png';
 
 export const InternationalComp = () => {
     return (
@@ -27,12 +29,15 @@ export const InternationalComp = () => {
                 <div className='internationalComp__section'>
                     <h1 data-aos="fade-up" data-aos-delay="200">Collaborations</h1>
                     <div className='internationalComp__innersection'>
-                        <img src={Img7} alt='' className='img__style7' data-aos="fade-up" data-aos-delay="100"/>
+                        {/* <img src={Img7} alt='' className='img__style7' data-aos="fade-up" data-aos-delay="100"/>
                         <img src={Img8} alt='' className='img__style8' data-aos="fade-up" data-aos-delay="200"/>
                         <img src={Img7} alt='' className='img__style7' data-aos="fade-up" data-aos-delay="300"/>
                         <img src={Img8} alt='' className='img__style8' data-aos="fade-up" data-aos-delay="400"/>
                         <img src={Img7} alt='' className='img__style7' data-aos="fade-up" data-aos-delay="500"/>
-                        <img src={Img8} alt='' className='img__style8' data-aos="fade-up" data-aos-delay="600"/>
+                        <img src={Img8} alt='' className='img__style8' data-aos="fade-up" data-aos-delay="600"/> */}
+
+                        <img src={meai} alt='' className='img__style8' data-aos="fade-up" data-aos-delay="600"/>
+                        <img src={imec} alt='' className='img__style8' data-aos="fade-up" data-aos-delay="600"/> 
                     </div>
                 </div>
             </div>
