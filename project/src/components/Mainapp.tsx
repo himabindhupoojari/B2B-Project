79b8da72dd4389f243e26a6a56d1f96d1fe59db0
anthonyveronica <anthonyveronica2000@gmail.com>
2024-11-03 14:45:59 +0530

    changes added

--- project/src/components/Mainapp.tsx
+++ project/src/components/Mainapp.tsx
@@ -11,18 +11,20 @@ export const Mainapp = () => {
             <div className='mainapp__section'>
                 <div className='mainapp__Sec1'><img src={Left} alt='' /></div>
                 <div className='mainapp__Sec'></div>
-                <div className='mainapp__Sec2'><img src={Right} alt='' /></div>
-                    {/* <img src={Left} alt='' /> */}
-                    {/* <img src={bannerM} alt='' /> */}
-                    {/* <img src={Right} alt='' /> */}
+                {/* <img src={Left} alt='' /> */}
+                {/* <img src={bannerM} alt='' /> */}
+                {/* <img src={Right} alt='' /> */}
                 {/* </div> */}
                 <div className='mainapp__innerSec'>
-                <img src={indiaJoyIcon} alt='indiaJoyIcon' className='indiajoy__img' data-aos="fade-up" data-aos-delay="100"/>
-                <img src={poweredByIcon} alt='poweredByIcon' className='poweredby__img' data-aos="fade-up" data-aos-delay="200"/>
-                <h1 data-aos="fade-up" data-aos-delay="300">IndiaJoy B2B Conclave</h1>
-                <p data-aos="fade-up" data-aos-delay="400">Meet and Pitch to leading Buyers, Broadcasters, Platforms & Studios.</p>
-                <p data-aos="fade-up" data-aos-delay="500">IndiaJoy B2B Conclave is a flagship initiative tailored to connect buyers, Broadcasters, Platforms, Co-Production partners with IP creators & Startup/Studios. Whether you are looking for selling your IP rights, Co-Production or want to scale your service offering business, IndiaJoy B2B conclave is the place to be.</p>
-                <button data-aos="fade-up" data-aos-delay="600">Register</button>
+                <div className='mainapp__Sec2'><img src={Right} alt='' /></div>
+                    <img src={indiaJoyIcon} alt='indiaJoyIcon' className='indiajoy__img' data-aos="fade-up" data-aos-delay="100" />
+                    <img src={poweredByIcon} alt='poweredByIcon' className='poweredby__img' data-aos="fade-up" data-aos-delay="200" />
+                    <h1 data-aos="fade-up" data-aos-delay="300">IndiaJoy B2B Conclave</h1>
+                    <p data-aos="fade-up" data-aos-delay="400">Meet and Pitch to leading Buyers, Broadcasters, Platforms & Studios.</p>
+                    <p data-aos="fade-up" data-aos-delay="500">IndiaJoy B2B Conclave is a flagship initiative tailored to connect buyers, Broadcasters, Platforms, Co-Production partners with IP creators & Startup/Studios. Whether you are looking for selling your IP rights, Co-Production or want to scale your service offering business, IndiaJoy B2B conclave is the place to be.</p>
+                    <a href='https://meai-imec.com/indiajoy/indiajoyRegister.html' target="_blank" rel="noopener noreferrer">
+                        <button data-aos="fade-up" data-aos-delay="600">Register</button>
+                    </a>
                 </div>
             </div>
         </div>
